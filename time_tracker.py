cffd4b4f61165b373dfd2d5b2a975f1e0808c373
obelisk <aaron.reyna@gmail.com>
2017-01-04 18:29:59 -0800

    changed length of samples to 1000

--- time_tracker.py
+++ time_tracker.py
@@ -3,15 +3,15 @@ import time, os, shutil, statistics
 
 x = 1
 
-local_file = "/media/test/0060.tif"
-remote_loc = "/media/151/CIFS_TEST/TIFS/0060.tif"
+local_file = "/media/test/1GBa.txt"
+remote_loc = "/media/151/CIFS_TEST/TIFS/1GBa.txt"
 
 x2 = 150
 y = 1
 
 while x2 < 160:
     print(x2)
-    to = str("/media/") + str(x2) + str("/CIFS_TEST/TIFS/0060.tif")
+    to = str("/media/") + str(x2) + str("/CIFS_TEST/TIFS/1GBa.txt")
     #print(to)
     x2 += 1
     #x2 = x2 + 1
@@ -20,22 +20,29 @@ while x2 < 160:
     varUP = []
     varDOWN = []
 
-    while x < 1000:
+    while x < 10:
+        os.system('dd if=/dev/urandom of=/media/test/1GBa.txt bs=64 count=16 iflag=fullblock')
         start_time = time.time()
-        if os.path.isfile("/media/test/0060.tif"):
+        if os.path.isfile("/media/test/1GBa.txt"):
             #print(to)
-            shutil.copy("/media/test/0060.tif", to)
+            shutil.copy("/media/test/1GBa.txt", to)
+            os.sync()
+            #print('I copied a file up')
         else:
             print("Error: %s file not found" % to)
         #uptime
+        #print("%s seconds" % (time.time() - start_time), "log A - UP", time.localtime())
         up.append(time.time() - start_time)
-        if os.path.isfile("/media/test/0060.tif"):
-            os.remove("/media/test/0060.tif")
+        if os.path.isfile("/media/test/1GBa.txt"):
+            os.remove("/media/test/1GBa.txt")
         else:
-            print("Error: %s file not found" % "/media/test/0060.tif")
+            print("Error: %s file not found" % "/media/test/1GBa.txt")
         start_time = time.time()
-        shutil.copy(to, "/media/test/0060.tif")
+        shutil.copy(to, "/media/test/1GBa.txt")
+        os.sync()
+        #print('I copied a file down')
         # down-time
+        #print("%s seconds" % (time.time() - start_time), "log B - DOWN", time.localtime())
         down.append(time.time() - start_time)
         x += 1
     print(up)
